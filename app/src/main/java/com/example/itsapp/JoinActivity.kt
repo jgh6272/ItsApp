bf613af68c0e90df5a41d2c87b0046612d8fa2f4
packmingyun <alsrbs12304@naver.com>
2021-04-23 23:08:21 +0900

    4/13 Retrofit 통신 Test

--- app/src/main/java/com/example/itsapp/JoinActivity.kt
+++ app/src/main/java/com/example/itsapp/JoinActivity.kt
@@ -3,6 +3,7 @@ package com.example.itsapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
@@ -10,16 +11,13 @@ import kotlinx.android.synthetic.main.activity_join.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import retrofit2.create
 
 class JoinActivity : AppCompatActivity() {
-
-    private lateinit var viewModel : JoinViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
-
-        viewModel = ViewModelProvider(this).get(JoinViewModel::class.java)
 
         back_btn.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
@@ -34,7 +32,6 @@ class JoinActivity : AppCompatActivity() {
             val userNickName = join_nick_name_edt.text.toString().trim()
 
             Toast.makeText(this,"회원가입 버튼 클릭",Toast.LENGTH_SHORT).show()
-            viewModel.join(userId, password, userName, userNickName)
         }
     }
 }
--- app/src/main/java/com/example/itsapp/JoinResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.example.itsapp
-
-import com.google.gson.annotations.SerializedName
-
-data class JoinResponse (
-    @SerializedName("code") var code : String
-)
