560803d5ed7e872741314737fec524583ec6d7a6
jgh6272 <jgh6272@naver.com>
2021-05-17 19:34:42 +0900

    5/17 HomeActivity.kt 하는 중

---
+++ app/src/main/java/com/example/itsapp/NewsFragment.kt
@@ -0,0 +1,17 @@
+package com.example.itsapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class NewsFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+}
--- app/src/main/java/com/example/itsapp/view/HomeActivity.kt
+++ app/src/main/java/com/example/itsapp/view/HomeActivity.kt
@@ -2,13 +2,20 @@ package com.example.itsapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import com.example.itsapp.HomeFragment
+import com.example.itsapp.MyPageFragment
+import com.example.itsapp.NewsFragment
 import com.example.itsapp.R
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_home.*
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     companion object{
         const val HOME_FRAGMENT = 0
         const val NEWS_FRAGMENT = 1
@@ -17,18 +24,31 @@ class HomeActivity : AppCompatActivity() {
     private lateinit var fragmentManager: FragmentManager
     private lateinit var  fragmentTransaction: FragmentTransaction
     private lateinit var homeFragment : HomeFragment
+    private lateinit var newsFragment: NewsFragment
+    private lateinit var myPageFragment: MyPageFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        fragmentManager = supportFragmentManager
-        homeFragment = HomeFragment()
-        initFragment(homeFragment)
+
     }
-    private fun initFragment(fragment: Fragment){
-        fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.addToBackStack(null)
-        fragmentTransaction.replace(R.id.container,fragment)
-        fragmentTransaction.commitAllowingStateLoss()
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when(p0.itemId){
+            R.id.home_fragment ->{
+                homeFragment = HomeFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.container,homeFragment).commit()
+            }
+            R.id.news_fragment ->{
+                newsFragment = NewsFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.container,newsFragment).commit()
+            }
+            R.id.mypage_fragment ->{
+                myPageFragment = MyPageFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.container,myPageFragment).commit()
+            }
+        }
+        return true
     }
+
 }
