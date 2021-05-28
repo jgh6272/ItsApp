3eb11ea197209a1803bf62b7cc43db4e8e58a6fb
jgh6272 <jgh6272@naver.com>
2021-05-28 15:07:47 +0900

    5/28 아이디/ 비번 찾기 하는중

--- app/src/main/java/com/example/itsapp/view/activity/FindPasswordActivity.kt
+++ app/src/main/java/com/example/itsapp/view/activity/FindPasswordActivity.kt
@@ -2,12 +2,7 @@ package com.example.itsapp.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.itsapp.FindPwFragment
 import com.example.itsapp.R
-import com.example.itsapp.view.FindAdapter
-import com.example.itsapp.view.fragment.FindIdFragment
-import com.google.android.material.tabs.TabLayout
-import kotlinx.android.synthetic.main.activity_find_password.*
 
 class FindPasswordActivity : AppCompatActivity() {
 
@@ -16,9 +11,6 @@ class FindPasswordActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_find_password)
 
-        val fragmentAdapter = FindAdapter(supportFragmentManager)
-        find_account_container.adapter = fragmentAdapter
 
-        tabs.setupWithViewPager(find_account_container)
     }
 }
