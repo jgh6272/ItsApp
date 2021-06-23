f41753aac117cd2bc2d2c95f83fae032786a4fc5
jgh6272 <jgh6272@naver.com>
2021-06-24 00:55:12 +0900

    6/24 Dialog 하는중

--- app/src/main/java/com/example/itsapp/view/activity/HomeActivity.kt
+++ app/src/main/java/com/example/itsapp/view/activity/HomeActivity.kt
@@ -1,8 +1,13 @@
 package com.example.itsapp.view.activity
 
+import android.app.Dialog
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import android.view.WindowManager
+import androidx.appcompat.app.AlertDialog
 import com.example.itsapp.view.fragment.HomeFragment
 import com.example.itsapp.view.fragment.MyPageFragment
 import com.example.itsapp.view.fragment.IssueFragment
@@ -21,11 +26,10 @@ class HomeActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         setContentView(R.layout.activity_home)
         bottom_navigation_view.setOnNavigationItemSelectedListener(this)
 
-
-
         // onCreate되면서 홈프래그먼트를 add로 바로 띄워준다.
         homeFragment = HomeFragment.newInstance()
         supportFragmentManager.beginTransaction().add(R.id.container,homeFragment).commit()
+        dialog()
     }
 
     override fun onNavigationItemSelected(p0: MenuItem): Boolean {
@@ -44,6 +48,20 @@ class HomeActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
             }
         }
         return true
+    }
+
+    fun dialog(){
+        /*val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_user_info)
+        dialog.window!!.setLayout(WindowManager.LayoutParams.MATCH_PARENT,WindowManager.LayoutParams.WRAP_CONTENT)
+        dialog.setCanceledOnTouchOutside(true)
+        dialog.setCancelable(true)
+        dialog.show()*/
+
+        val dialog2 = AlertDialog.Builder(this)
+        dialog2.setView(R.layout.dialog_user_info)
+        dialog2.show()
+
     }
 
 }
